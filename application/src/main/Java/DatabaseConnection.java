0b0b04615bcd9cfa8b68d5d65905e5ba76f6d584
Tizziana <Tizziana.Mansy@gmail.com>
2023-10-13 11:14:37 +0200

    tables

--- application/src/main/Java/DatabaseConnection.java
+++ application/src/main/Java/DatabaseConnection.java
@@ -10,6 +10,31 @@ public class DatabaseConnection {
     private static final String PASSWORD = "roh7iuSo";
     private static Connection connection;
 
+    public String createTableUsers() {
+        String createUser = "CREATE TABLE IF NOT EXISTS Users ("
+                + "idUsers VARCHAR(20) PRIMARY KEY,"
+                + "firstName VARCHAR(20) NOT NULL,"
+                + "lastName VARCHAR(20) NOT NULL,"
+                + "email VARCHAR(40) NOT NULL,"
+                + "password VARCHAR(20) NOT NULL"
+                +")";
+        return createUser;
+
+        public String createTableHelp_Seeker() {
+            String createHelp_Seeker = "CREATE TABLE IF NOT EXISTS Help_Seekers ("
+                    + "idUsers VARCHAR(20) REFERENCES Users(idUsers) ON DELETE CACASDE"
+                    +")";
+            return createHelp_Seeker;
+    }
+
+        public String createTableVolunteer() {
+            String createVolunteer = "CREATE TABLE IF NOT EXISTS Volunteers ("
+                    + "idUsers VARCHAR(20) REFERENCES Users(idUsers) ON DELETE CACASDE"
+                    +")";
+            return createHelp_Seeker;
+        }
+    }
+
     // Private constructor to prevent instantiation from outside the class
     private DatabaseConnection() {
     }
