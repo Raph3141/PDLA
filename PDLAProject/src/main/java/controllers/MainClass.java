b9d4eaf055b5f3832d33d2aa21246fcdf19cdd93
Raph <rees@insa-toulouse.fr>
2023-10-17 16:50:29 +0200

    test user

--- PDLAProject/src/main/java/controllers/MainClass.java
+++ PDLAProject/src/main/java/controllers/MainClass.java
@@ -1,15 +1,17 @@
 package controllers;
-
+import model.*;
 import controllers.DatabaseConnection;
-
+import model.Users;
 import java.sql.Connection;
 public class MainClass {
         public static void main(String[] args) {
-        Connection connection = DatabaseConnection.getConnection();
+            Users foo = new Users("001","fn","ln","foo@email.com","pw");
+            System.out.println("MY NAME IS "+foo.getFirstName());
+            Connection connection = DatabaseConnection.getConnection();
+            // Use the connection for database operations
 
-        // Use the connection for database operations
 
-        // Don't forget to close the connection when done
-        DatabaseConnection.closeConnection();
+            // Don't forget to close the connection when done
+            DatabaseConnection.closeConnection();
     }
 }
