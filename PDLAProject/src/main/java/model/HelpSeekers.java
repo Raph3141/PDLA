f03290bb9c0fd9d9b8cfa3dd6feb2f28a95e7cf7
Raph <rees@insa-toulouse.fr>
2023-10-17 16:39:03 +0200

    helpseekers

--- PDLAProject/src/main/java/model/HelpSeekers.java
+++ PDLAProject/src/main/java/model/HelpSeekers.java
@@ -1,8 +1,11 @@
 package model;
-import model.*
-public class HelpSeekers extends Users{
-    public HelpSeeker (String id,String firstName,String lastName,String email,String password){
+
+import model.Users;
+
+public class HelpSeekers extends Users {
+
+    public HelpSeekers (String id,String firstName,String lastName,String email,String password){
         super (id, firstName, lastName,email,password);
+
     }
 }
-
