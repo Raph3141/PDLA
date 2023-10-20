2a0d860afb4be313f6a5886877b2a6f6674c4b83
Tizziana <Tizziana.Mansy@gmail.com>
2023-10-20 17:57:14 +0200

    view

--- PDLAProject/src/main/java/controllers/MainClass.java
+++ PDLAProject/src/main/java/controllers/MainClass.java
@@ -1,8 +1,12 @@
 package controllers;
 import model.*;
 import controllers.DatabaseConnection;
-import model.Users;
+import model.*;
+import view.*;
 import java.sql.Connection;
+
+import static view.class1.CreateAndShowWindow;
+
 public class MainClass {
         public static void main(String[] args) {
             Users foo = new Users("001","fn","ln","foo@email.com","pw");
@@ -10,6 +14,13 @@ public class MainClass {
             Connection connection = DatabaseConnection.getConnection();
             // Use the connection for database operations
 
+            //run view
+            /*javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                   class1.CreateAndShowWindow();
+                }
+            });*/
 
             // Don't forget to close the connection when done
             DatabaseConnection.closeConnection();
--- PDLAProject/src/main/java/view/class1.java
+++ PDLAProject/src/main/java/view/class1.java
@@ -1,4 +1,13 @@
 package view;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class class1 { //test
+    public static void CreateAndShowWindow(){
+        //create and set up the window
+        JFrame frame = new JFrame("Interface");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //to exit the window
+    }
 }
