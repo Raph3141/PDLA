a239d397ee65c9bf3279df56126be2692819d628
Tizziana <Tizziana.Mansy@gmail.com>
2023-11-15 20:27:10 +0100

    started volunteerpage

--- PDLAProject/src/main/java/view/VolunteerPage.java
+++ PDLAProject/src/main/java/view/VolunteerPage.java
@@ -1,12 +1,46 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class VolunteerPage {
 
-    VolunteerPage(String Volunteerid){
+    VolunteerPage(String Volunteerid) {
+
         NewFrame VolunteerFrame = new NewFrame();
 
-        JLabel LocationLabel = new JLabel("Where do I need help: ");
+        // Retrieve requests from the database
+        List<model.Request> requests = GetRequests();
+
+        // Create a panel to hold the list of requests
+        JPanel RequestPanel = new JPanel();
+        RequestPanel.setLayout(new BoxLayout(RequestPanel, BoxLayout.Y_AXIS));
+
+        // Add each request to the panel
+        for (model.Request request : requests) {
+            JLabel RequestLabel = new JLabel("Date of the request: " + request.getDate() + ", Location: " + request.getLocation() + "Description of the request :" + request.getDescription());
+            RequestPanel.add(RequestLabel);
+
+            //choose a request
+            JButton ChooseButton = new JButton("Select this request.");
+            ChooseButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    // Add volunteer's ID to the selected request
+
+
+                    // Provide feedback to the user
+                    JOptionPane.showMessageDialog(null, "You are now assigned to this request.");
+        }
+
+        //show the requests
+        VolunteerFrame.setLayout(new BorderLayout());
+        JScrollPane RequestPane = new JScrollPane(RequestPanel);
+        VolunteerFrame.add(RequestPane, BorderLayout.CENTER);
     }
 }
+
